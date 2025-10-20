36f3ae401d9c0cb001718220cf4e13ddfaf05074
kelly deal <kellyanndeal@gmail.com>
2025-10-19 20:39:16 -0400

    some styling changes

---
+++ smart-wardrobe/.netlify/functions-serve/weather/netlify/functions/weather.js
@@ -0,0 +1,45 @@
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+
+// netlify/functions/weather.js
+var weather_exports = {};
+__export(weather_exports, {
+  handler: () => handler
+});
+module.exports = __toCommonJS(weather_exports);
+async function handler(event) {
+  const key = process.env.WEATHER_API;
+  try {
+    const res = await fetch(`https://weather.googleapis.com/v1/currentConditions:lookup?key=${key}&location.latitude=39.1031&location.longitude=-84.5120&unitsSystem=IMPERIAL`);
+    const data = await res.json();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data)
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: err.message })
+    };
+  }
+}
+// Annotate the CommonJS export names for ESM import in node:
+0 && (module.exports = {
+  handler
+});
+//# sourceMappingURL=weather.js.map
--- smart-wardrobe/src/App.svelte
+++ smart-wardrobe/src/App.svelte
@@ -1,6 +1,6 @@
 <script>
-  import ClothesPicker from "./lib/ClothesPicker.svelte";
   import { tops } from "./data";
+  import ClothesPicker from "./lib/ClothesPicker.svelte";
 
   function test() {
     tops[0].inWardrobe = false;
@@ -14,25 +14,26 @@
     isOpen = false;
   }
 
-  let isOutfitsDisplayed = $state(true)
-  let isWeatherDisplayed = $state(false)
+  let isOutfitsDisplayed = $state(true);
+  let isWeatherDisplayed = $state(false);
   function toggleOutfits() {
-    isOutfitsDisplayed = true
-    isWeatherDisplayed = false
+    isOutfitsDisplayed = true;
+    isWeatherDisplayed = false;
   }
 
-  function toggleWeather(){
-    isOutfitsDisplayed = false
-    isWeatherDisplayed = true
+  function toggleWeather() {
+    isOutfitsDisplayed = false;
+    isWeatherDisplayed = true;
 
-    getWeather()
+    getWeather();
   }
 
-  let currTemp = $state(0)
-  let maxTemp = $state(0)
-  let minTemp = $state(0)
-  function getWeather(){
-    let request = "http://api.weatherapi.com/v1/forecast.json?key=b853acd0326a4155bde181823251710&q=Cincinnati&days=1&aqi=no&alerts=no"
+  let currTemp = $state(0);
+  let maxTemp = $state(0);
+  let minTemp = $state(0);
+  function getWeather() {
+    let request =
+      "http://api.weatherapi.com/v1/forecast.json?key=b853acd0326a4155bde181823251710&q=Cincinnati&days=1&aqi=no&alerts=no";
 
     fetch(request, {
       mode: "cors",
@@ -44,65 +45,98 @@
         return response.json();
       })
       .then((responseData) => {
-        currTemp = responseData.current.temp_f
-        maxTemp = responseData.forecast.forecastday[0].day.maxtemp_f
-        minTemp = responseData.forecast.forecastday[0].day.mintemp_f
+        currTemp = responseData.current.temp_f;
+        maxTemp = responseData.forecast.forecastday[0].day.maxtemp_f;
+        minTemp = responseData.forecast.forecastday[0].day.mintemp_f;
       });
   }
 </script>
 
 <main>
   <div class="full-page">
-    <div>
-      <button onclick={toggleOutfits}>Saved Outfits</button>
+    <div class="side-view">
+      <button onclick={toggleOutfits} class="main-button">Saved Outfits</button>
       <button onclick={toggleWeather}>Weather</button>
       <div hidden={!isOutfitsDisplayed}>
         <h1>Saved Outfits</h1>
+
+        <div class="">
+
+        </div>
       </div>
       <div hidden={!isWeatherDisplayed}>
         <h1>Weather</h1>
         <div>
-<a class="weatherwidget-io" href="https://forecast7.com/en/39d10n84d51/cincinnati/?unit=us" data-label_1="CINCINNATI" data-label_2="WEATHER" data-mode="Current" data-theme="original" >CINCINNATI WEATHER</a>
-<script>
-!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
-</script>
+          <a
+            class="weatherwidget-io"
+            href="https://forecast7.com/en/39d10n84d51/cincinnati/?unit=us"
+            data-label_1="CINCINNATI"
+            data-label_2="WEATHER"
+            data-mode="Current"
+            data-theme="original">CINCINNATI WEATHER</a
+          >
+          <script>
+            !(function (d, s, id) {
+              var js,
+                fjs = d.getElementsByTagName(s)[0];
+              if (!d.getElementById(id)) {
+                js = d.createElement(s);
+                js.id = id;
+                js.src = "https://weatherwidget.io/js/widget.min.js";
+                fjs.parentNode.insertBefore(js, fjs);
+              }
+            })(document, "script", "weatherwidget-io-js");
+          </script>
         </div>
-        <p>Today has a high of {maxTemp} and a low of {minTemp}. Consider bringing a jacket for the morning.</p>
+        <p>
+          Today has a high of {maxTemp} and a low of {minTemp}. Consider
+          bringing a jacket for the morning.
+        </p>
       </div>
     </div>
     <div class="main-view">
-      <ClothesPicker type="tops" />
+      <div style="display: inline-block">
+        <ClothesPicker type="tops" />
 
-      <ClothesPicker type="bottoms" />
+        <ClothesPicker type="bottoms" />
 
-      <ClothesPicker type="shoes" />
+        <ClothesPicker type="shoes" />
 
-      <button onclick={test}>Select Outfit</button>
+        <button onclick={test} class="main-button">Select Outfit</button>
+      </div>
     </div>
     <div class="testing">
       <div class="Header">
         <h1>Testing UI</h1>
-        <div class ="info">
-          <button style="margin-left: 200px;" onclick= {() => alert("The smart wardrobe has touchscreens on each side and center of it. Select your choices. Once selected, the bottom drawer will automatically update and give you your selected outfit.")} > 
-            Information </button>
+        <div class="info">
+          <button
+            style="margin-left: 200px;"
+            onclick={() =>
+              alert(
+                "The smart wardrobe has touchscreens on each side and center of it. Select your choices. Once selected, the bottom drawer will automatically update and give you your selected outfit."
+              )}
+          >
+            Information
+          </button>
         </div>
-      <div class = "interactTest">
-        {#if !isOpen}
-        <img src="/closed_wardrobe.png" alt="Closed wardrobe" />
+        <div class="interactTest">
+          {#if !isOpen}
+            <img src="/closed_wardrobe.png" alt="Closed wardrobe" />
 
-        <div class = "stimulate">
-          <button onclick={stimulate}>Stimulate</button>
+            <div class="stimulate">
+              <button onclick={stimulate}>Stimulate</button>
+            </div>
+          {:else}
+            <img src="/open-wardrobe.png" alt="Open wardrobe" />
+            <p>The wardrobe is now open. This week:</p>
+            <div>
+              <p>8 clothing needs laundry</p>
+              <p>4 preset saved outfits worn</p>
+              <p>3 new outfits design worn</p>
+            </div>
+            <button onclick={reset}>Close Wardrobe</button>
+          {/if}
         </div>
-        {:else}
-          <img src="/open-wardrobe.png" alt="Open wardrobe" />
-          <p>The wardrobe is now open. This week:</p>
-          <div>
-            <p>8 clothing needs laundry</p>
-            <p>4 preset saved outfits worn</p>
-            <p>3 new outfits design worn</p>
-          </div>
-          <button onclick={reset}>Close Wardrobe</button>
-        {/if}
       </div>
     </div>
   </div>
